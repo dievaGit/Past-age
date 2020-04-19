4f477866f3078bcf6a5b263c101b2f0b868ca386
Dieva Leon <58702512+dievaGit@users.noreply.github.com>
2020-04-18 23:21:16 -0400

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EdadPasada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write(" Escriba su edad:");
+            int age = Convert.ToInt32(Console.ReadLine());
+            int oldAge = 10 + age;
+            Console.WriteLine(" Su edad dentro de años es: {0}", oldAge);
+
+            Console.ReadKey();
+        }
+    }
+}
